a2b609d46117ec3932c51248beabdb119b0834b0
EskoSalaka <esko.salaka@gmail.com>
2017-12-29 18:49:35 +0200

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,129 @@
+import math
+import numpy
+from squares import paint, create_squares
+
+
+########################################################################################################################
+# Some handy math functions
+########################################################################################################################
+def is_prime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    return 1 if all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2)) else 0
+
+
+def fib(n):
+    return int((1 + math.sqrt(5))**n - (1 - math.sqrt(5))**n) / (2**n*math.sqrt(5))
+
+
+def is_fibonacci(n):
+    phi = 0.5 + 0.5 * math.sqrt(5.0)
+    a = phi * n
+    return n == 0 or abs(round(a) - a) < 1.0 / n
+
+
+def ulam_numbers(a, b, n):
+    U = [a, b]
+    i = 2
+
+    for _ in [[0]] * (n - 2):
+        t = _ * 3 * i
+
+        for a in U:
+            for b in U:
+                t[a + b] += a != b
+        i = t[i + 1:].index(2) + i + 1
+        U += [i]
+
+    return U
+
+
+def divisor_sum(n):
+    sum([i for i in range(1, n) if n % i == 0])
+
+
+########################################################################################################################
+# Some stuff. Feel free to add your own.
+########################################################################################################################
+white = [255, 255, 255]
+black = [0, 0, 0]
+
+# Primes of the first 1 000 000 numbers as squares.
+# s = create_squares('none', 1000, 1000, lambda array, n, h, x, *extras: is_prime(n))
+# paint(s, black, white, 'primes_1000x1000.BMP')
+
+
+# An interesting kind of Sierpinski-triangles: Paint the square if the sum of the number of painted squares of all the
+# above rows, starting from each row's current column index is even.
+# s = create_squares('ones', 1000, 1000, lambda array, n, h, x, *extras: array[:h, x:].sum() % 2)
+# paint(s, black, white, 'triangles_1000x1000.BMP')
+
+# Same, but with a random first line
+# s = create_squares('random', 1000, 1000, lambda array, n, h, x, *extras: array[:h, x:].sum() % 2)
+# paint(s, black, white, 'triangles_random_1000x1000.BMP')
+
+# Suprisingly, it's hard to see any pattern if we do the same as above but check for divisibility by 3
+# s = create_squares('ones', 1000, 1000, lambda array, n, h, x, *extras: 1 if array[:h, x:].sum() % 3 == 0 else 0)
+# paint(s, black, white, 'div_3_1000x1000.BMP')
+
+# And again the same with a random first line
+# s = create_squares('random', 1000, 1000, lambda array, n, h, x, *extras: 1 if array[:h, x:].sum() % 3 == 0 else 0)
+# paint(s, black, white, 'div_3_1000x1000.BMP')
+
+# Color the square if the three above squares are mirror-symmetric starting with a random first line
+# def mirror_symmetric_3(array, n, h, x, *extras):
+#     a = array[h - 1][x - 1]
+#     c = array[h - 1][x + 1] if x + 1 < array[h - 1].size else array[h - 1][0]
+#     return 1 if (a and c) or (not a and not c) else 0
+#
+#
+# s = create_squares('random', 1000, 1000, mirror_symmetric_3)
+# paint(s, black, white, 'mirror_symmetric_3_1000x1000.BMP')
+
+
+# The same as above, but with 5 above squares
+# def mirror_symmetric_5(array, n, h, x, *extras):
+#     a = array[h - 1][x - 2]
+#     b = array[h - 1][x - 1]
+#     d = array[h - 1][x + 1] if x + 1 < array[h - 1].size else array[h - 1][0]
+#     e = array[h - 1][x + 2] if x + 2 < array[h - 1].size else array[h - 1][1]
+#     return 1 if (a and b and d and e) or (not a and not b and not d and not e) or \
+#                 (a and not b and not d and e) or (not a and b and d and not e) else 0
+#
+#
+# s = create_squares('random', 1000, 1000, mirror_symmetric_5)
+# paint(s, black, white, 'mirror_symmetric_5_1000x1000.BMP')
+
+
+# Color the square if exactly one of the two above corner squares are colored
+# def one_corner(array, n, h, x, *extras):
+#     a = array[h - 1][x - 1]
+#     c = array[h - 1][x + 1] if x + 1 < array[h - 1].size else array[h - 1][0]
+#     return (a + c) % 2
+#
+#
+# s = create_squares('random', 1000, 1000, one_corner)
+# paint(s, black, white, 'one_corner_1000x1000.BMP')
+
+# Same as above, but with a first line of zeros and a single one
+# def one_corner(array, n, h, x, *extras):
+#     a = array[h - 1][x - 1]
+#     c = array[h - 1][x + 1] if x + 1 < array[h - 1].size else array[h - 1][0]
+#     return (a + c) % 2
+#
+#
+# cl = numpy.zeros(1000)
+# cl[500] = 1
+# s = create_squares('custom', 1000, 1000, one_corner, custom_first_line=cl)
+# paint(s, black, white, 'one_corner_1000x1000.BMP')
+
+# The Ulam numbers of the first 1000 numbers. This way of generating Ulam numbers is not particularly great.
+# ulam_num = ulam_numbers(1, 2, 900)
+# ul1 = create_squares('none', 100, 100, lambda array, n, h, x, *extras: 1 if n in extras[0] else 0, *ulam_num)
+# paint(ul1, [0, 0, 0], [255, 255, 255], 'ulam1_100x100.BMP')
+
+
+# The binary representation of the first 1000 Ulam numbers
+# ulam_num = ulam_numbers(1, 2, 1000)
+# s = create_squares('none', 15, 1000, lambda array, n, h, x, *extras: (extras[0][h] & (1 << x)) >> x, *ulam_num)
+# paint(s, black, white, 'ulam_binaries_15x1000.BMP')
---
+++ squares.py
@@ -0,0 +1,122 @@
+import sys
+import numpy
+
+from PIL import Image
+
+
+def update_progress(n, tot):
+    """A simple progress indicator."""
+    sys.stdout.write("\r[{} / {}]".format(n, tot))
+
+
+def to_pixel_array(array, square_color, background_color):
+    """
+    Converts the numpy 2d-array of 1's and 0's to a image data-array. The data array will simply have the given
+    RGB-color array [R, G, B], square_color, in place of ones and the given background_color in place of zeros.
+
+    :param array: A numpy 2d-array of 1's and 0's
+    :param square_color: An RGB color array to paint the squares
+    :param background_color: An RGB color array to paint the background
+    :return: An image data-array
+    """
+    pixel_array = numpy.zeros((array.shape[0], array.shape[1], 3), dtype=numpy.uint8)
+
+    for y in range(len(pixel_array)):
+        for x in range(len(array[y])):
+            if array[y][x]:
+                pixel_array[y][x] = square_color
+            else:
+                pixel_array[y][x] = background_color
+
+    return pixel_array
+
+
+def create_squares(first_line, width, height, painter_func, *func_extras, custom_first_line=None, progress=True):
+    """
+    Creates an array of 1's and 0's of given width and height where 1's represent painted squares. The squares are
+    painted according to a given function 'painter_func'. he structure of the 'painter_func' is described in more detail
+    later.Basically, each 'square', or element of the array, is iterated and passed to the 'painter_func', which in
+    turn either paints the square according to its logic. It is used in the following way:
+    squares_iterator[0] = painter_func(squares, n, h, x, func_extras)
+
+    Optinally a first line, or row, can be specified with 'first_line'. The legal arguments for 'first_line' are:
+    'random' -- fills the first line randomly
+    'ones' -- fills the first line with ones
+    'zeros' -- fills the first line with zeros.
+    'custom' -- fills the first line with the given 'custom_first_line'
+    'none' -- leaves the first line empty.
+
+    In case the 'first_line' is something other than 'none', the iteration process will begin from the second line!
+
+    :param first_line: What kind of first line, or row, to have. Either 'random', 'ones', 'zeros', 'custom' or 'none'
+    :param width: The width of the canvas in squares (pixels)
+    :param height: The height of the canvas in squares (pixels)
+    :param painter_func: The painter function which paints the squares according to some logic
+    :param custom_first_line: In case 'custom' was specified as the 'first line', this will be set as the first line
+    :param progress: Boolean value to specify if a simple progress message is displayed while iterating
+    :param func_extras: Any extras the painter_func needs, leave empty otherwise
+    :return: A numpy 2d-array of 1's and 0's representing painted and non-painted squares
+    """
+    squares = numpy.zeros((height, width), dtype=numpy.uint8)
+    squares_iterator = numpy.nditer(squares, op_flags=['writeonly'], flags=['multi_index'])
+
+    if first_line == 'random':
+        for _ in range(width):
+            squares_iterator[0] = numpy.random.randint(0, 2)
+            squares_iterator.iternext()
+    elif first_line == 'zeros':
+        for _ in range(width):
+            squares_iterator.iternext()
+    elif first_line == 'ones':
+        for _ in range(width):
+            squares_iterator[0] = 1
+            squares_iterator.iternext()
+    elif first_line == 'custom':
+        for _ in range(width):
+            squares_iterator[0] = custom_first_line[squares_iterator.multi_index[1]]
+            squares_iterator.iternext()
+    elif first_line == 'none':
+        pass
+    else:
+        print('Wrong positional argument for "first_line"')
+
+    tot = width * height
+    while not squares_iterator.finished:
+        h = squares_iterator.multi_index[0]
+        x = squares_iterator.multi_index[1]
+        n = width * h + x + 1
+
+        squares_iterator[0] = painter_func(squares, n, h, x, func_extras)
+
+        if progress:
+            update_progress(n, tot)
+
+        squares_iterator.iternext()
+
+    return squares
+
+
+def paint(array, square_color, background_color, save_loc='squares.BMP'):
+    """Converts the given numpy 2d-array to an image data-array and creates an image from it."""
+    image = Image.fromarray(to_pixel_array(array, square_color, background_color))
+    image.save(save_loc)
+    image.show()
+
+
+def painter_func_demo(array, n, h, x, *extras):
+    """
+    A simple demostration of how the painter func works. Generally, these functions require the 2d-numpy array itself,
+    the current square number n, the row number h, the current column index x and a list of extras, which can be
+    anything. They always return either 1 or 0 representing a painted or non-painted square.
+
+    This one simply paints the square if the square number n is even.
+
+    :param array: A numpy 2d-array of 1's and 0's
+    :param n: The current square number
+    :param h: The current row
+    :param x: The current column index
+    :param extras: A list of any extra the func needs
+    :return: Either 1 to paint the square or 0 to leave it empty
+    """
+
+    return 1 if n % 2 == 0 else 0
